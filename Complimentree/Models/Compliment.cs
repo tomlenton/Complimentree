531721f23a3f4654f865c3d17bd7d56c589e5c0a
Ethamoto <75836406+Ethamoto@users.noreply.github.com>
2026-05-14 13:54:50 +0100

    First Commit

---
+++ Complimentree/Models/Compliment.cs
@@ -0,0 +1,34 @@
+﻿namespace Complimentree.Models
+{
+    public class Compliment
+    {
+        public int Id { get; set; }
+        public string Text { get; set; } = string.Empty;
+
+
+        public static Compliment GetCompliment()
+        {
+            List<string> ComplimentsAsStrings = new List<string>{
+                "You may not be evergreen, but you are to me.",
+                "I bet squirrels consider you prime real estate.",
+                "You're so down to earth.",
+                "You're very grounded.",
+                "Your leaves are soothing.",
+                "You sway nicely in the wind.",
+                "You're blooming brilliant.",
+                "You're really good at standing still and doing nothing.",
+                "Let's get to the root of your beauty.",
+                "I will never leaf you.",
+                "You've really branched out lately.",
+                "You provide the perfect amount of shade on a hot day.",
+                "I'm really pine-ing for you." };
+
+            Random rnd = new Random();
+            int complimentIndex = rnd.Next(0, ComplimentsAsStrings.Count);
+            Compliment complimnetToReturn = new Compliment() { Id = complimentIndex, Text = ComplimentsAsStrings[complimentIndex] };
+            return complimnetToReturn;
+        }
+    }
+
+  
+}
---
+++ Complimentree/Models/Tree.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+
+namespace Complimentree.Models
+{
+    public class Tree
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public Location? Location { get; set; }
+
+        public double Size = double.NaN;
+        public string Species { get; set; } = string.Empty;
+        public List<Compliment> Compliments { get; set; } = new List<Compliment>();
+    
+
+      //public static string ComplimentsAsString()
+      //  {
+      //      string listToReturn = string.Empty;
+      //      foreach (var compliment in Compliments)
+      //      {
+      //          listToReturn += $"{compliment.Text}, ";
+      //      }
+
+      //      return listToReturn = listToReturn.Substring(0, listToReturn.Length - 2);
+
+      //  }
+    }
+}
